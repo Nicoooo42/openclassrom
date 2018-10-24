f0219effcedef1903875f81217cb80fef2f0f3cc
Nicolas <nicolas.lamanna@wanadoo.fr>
2018-10-25 01:19:41 +0200

    gherg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'openclassrom';
+    constructor() {
+        const config = {
+            apiKey: 'AIzaSyDNukowBQMqolWnfanJhwJZx7cCg65ObT0',
+            authDomain: 'ckoipapa-220414.firebaseapp.com',
+            databaseURL: 'https://ckoipapa-220414.firebaseio.com',
+            projectId: 'ckoipapa-220414',
+            storageBucket: 'ckoipapa-220414.appspot.com',
+            messagingSenderId: '508799242944'
+        };
+        firebase.initializeApp(config);
+    }
 }
